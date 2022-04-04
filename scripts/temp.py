c83eafcc6ee39ab9b028206b944fa4b4fab96728
Koe2200 <79816353+Koe2200@users.noreply.github.com>
2022-04-04 16:51:06 +1200

    Added base GUI menu with drop down menu bar

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,1 +1,27 @@
-print("This is a temp file")
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QStatusBar
+from PyQt5.QtWidgets import QToolBar
+
+class Window(QMainWindow):
+    """Main Window."""
+    def __init__(self, parent=None):
+        """Initializer."""
+        super().__init__(parent)
+        self.setWindowTitle('Handwritten Digit/English Recognizer')
+        self._createMenu()
+
+    def _createMenu(self):
+        self.menu = self.menuBar().addMenu("&File")
+        self.menu.addAction('&Quit', self.close)
+        self.menu = self.menuBar().addMenu("&View")
+        
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec_())
