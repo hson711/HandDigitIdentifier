fde78391f022ebd1eac1be312a203129839c51f3
Koe2200 <79816353+Koe2200@users.noreply.github.com>
2022-04-28 03:23:11 +1200

    Made customPainter.py result in drawings similar

--- scripts/customPainter.py
+++ scripts/customPainter.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow, QAction, qApp
-from PyQt5 import QtCore, uic
+from PyQt5 import QtCore, uic, QtWidgets
 from PyQt5 import QtGui
 from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtCore import Qt
@@ -14,35 +14,35 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp
 from PyQt5.QtGui import QIcon
-
-class customPainter(QWidget):
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+class customPainter(QtWidgets.QLabel):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
         self.points = QtGui.QPolygon()
-        self.clear = False
-        self.setGeometry(300, 300, 400, 300)
+        pixmap = QtGui.QPixmap(600,300)
+        self.setPixmap(pixmap)
         self.setWindowTitle('Custom Painter')
+        pal = self.palette()
+        pal.setColor(QPalette.Background, Qt.black)
+        self.setAutoFillBackground(True)
+        self.setPalette(pal)
         self.show()
     
     def paintEvent(self, e):
-        qp = QPainter()
+        qp = QtGui.QPainter()
         qp.begin(self)
         self.draw_point(qp)
-        if self.clear:
-            canvas = QtGui.QPixmap(400, 300)
-            canvas.fill(QtGui.QColor('#ffffff'))
-            print("hi")
-            self.clear = False
         qp.end()
 
-    def clearPaint(self):
-        self.update()
 
     def draw_point(self, qp):
-        qp.setPen(QPen(Qt.black,  5))
+        qp.setPen(QPen(Qt.white,  20))
         qp.drawPoints(self.points)
 
     def mouseMoveEvent(self, e):
@@ -64,7 +64,7 @@ class SecondExample(QDialog):
         toolbar = QMenuBar()
         paintClearAction = QAction("Clear Paint", toolbar)
         toolbar.addAction(paintClearAction)
-        paintClearAction.triggered.connect(self.widget.clearPaint)
+        #paintClearAction.triggered.connect(self.widget.clearPaint)
 
 
         layout.setMenuBar(toolbar)
