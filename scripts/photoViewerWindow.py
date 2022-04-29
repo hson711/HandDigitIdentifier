19cb1afd4a1204628738c45a2a4965cf1778b74a
Koe2200 <79816353+Koe2200@users.noreply.github.com>
2022-04-30 07:08:12 +1200

    Finished photoViewerWindow

--- scripts/photoViewerWindow.py
+++ scripts/photoViewerWindow.py
@@ -1,4 +1,5 @@
 from asyncio.windows_events import NULL
+from pickle import TRUE
 from PyQt5.QtWidgets import (QWidget, QSlider, QLineEdit, QLabel, QPushButton, QScrollArea,QApplication,QHBoxLayout, QVBoxLayout, QMainWindow)
 from PyQt5.QtCore import Qt, QSize
 from PyQt5 import QtWidgets, uic
@@ -10,20 +11,22 @@ import io
 from PIL import Image, ImageCms
 import os
 from DNNFunctions import DNNFunctions
+import threading
 
 class photoViewerWindow(QMainWindow):
     def __init__(self,set):
         super().__init__()
-        self.initUI()
-        self.set = set
-        self.photoViewer()
+        self.initUI(set)
 
-    def initUI(self):
+    def initUI(self,set):
         self.scroll = QScrollArea()
         self.widget = QWidget() 
         self.layout = QGridLayout()
         self.i = 0
         self.j = 0
+        self.z = 0
+        self.layout.setSpacing(0)
+        self.set = set
 
         toolbar = QMenuBar()
         filterPhotosAction = QAction("Filter", toolbar)
@@ -38,39 +41,74 @@ class photoViewerWindow(QMainWindow):
         self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.scroll.setWidgetResizable(True)
         self.scroll.setWidget(self.widget)
+        self.scrollBar = self.scroll.verticalScrollBar()
+        self.scrollBar.valueChanged.connect(self.scrolled)
 
         self.setCentralWidget(self.scroll)
 
-        self.setGeometry(600, 100, 1200, 1200)
+        self.setGeometry(600, 100, 933, 600)
+        self.setFixedSize(933,600)
         self.setWindowTitle('Scroll Area Demonstration')
         self.show()
+        self.loadData()
+
+    
+    def scrolled(self, value):
+        tempValue = (self.scrollBar.maximum())
+        maxValue = round(tempValue * 1)
+        minValue = round(tempValue * 0.95)
+        halfValue = round(tempValue * 0.5)
+        if value >= minValue and value <= maxValue :
+            self.showPhotoExsisting(self.lengthx, self.datax)
 
 
-    def showPhoto(self, lengthx, lengthy, datax, datay):
-        for i in range(100):
-            pm1 = DNNFunctions.convertNumpyArrayToImage(datax[i])
+    def showPhoto(self, lengthx, datax):
+        temp = self.z
+        while self.z <= temp + 1000:
+            pm1 = DNNFunctions.convertNumpyArrayToImage(datax[self.z])
             if not pm1.isNull():
                 imageLabel = QLabel(self)
                 imageLabel.setPixmap(pm1)
-                if self.j == 50:
+                if self.j == 32:
                     self.j = 0
                     self.i = self.i + 1
+                self.layout.setColumnStretch(self.j,1)
+                self.layout.setRowStretch(self.i,1)
                 self.layout.addWidget(imageLabel,self.i,self.j)
                 self.j = self.j + 1
+                self.z = self.z + 1
     
+    def showPhotoExsisting(self, lengthx, datax):
+        datax = DNNFunctions.raw_train_x
+        temp = self.z
+        while self.z <= temp + 32:
+            pm1 = DNNFunctions.convertNumpyArrayToImage(datax[self.z])
+            if not pm1.isNull():
+                imageLabel = QLabel(self)
+                imageLabel.setPixmap(pm1)
+                if self.j == 32:
+                    self.j = 0
+                    self.i = self.i + 1
+                self.layout.setColumnStretch(self.j,1)
+                self.layout.setRowStretch(self.i,1)
+                self.layout.addWidget(imageLabel,self.i,self.j)
+                self.j = self.j + 1
+                self.z = self.z + 1
     
-    def photoViewer(self):
+    def loadData(self):
         if (self.set == 'Train Set'):
-            lengthx = len(DNNFunctions.raw_train_x)
-            datax = DNNFunctions.raw_train_x
-            lengthy = len(DNNFunctions.raw_train_y)
-            datay = DNNFunctions.raw_train_y
+            self.lengthx = len(DNNFunctions.raw_train_x)
+            self.datax = DNNFunctions.raw_train_x
+            self.lengthy = len(DNNFunctions.raw_train_y)
+            self.datay = DNNFunctions.raw_train_y
         else:
-            lengthx = len(DNNFunctions.raw_test_x)
-            datax = DNNFunctions.raw_test_x
-            lengthy = len(DNNFunctions.raw_test_y)
-            datay = DNNFunctions.raw_test_y
-        self.showPhoto(lengthx, lengthy, datax, datay)
+            self.lengthx = len(DNNFunctions.raw_test_x)
+            self.datax = DNNFunctions.raw_test_x
+            self.lengthy = len(DNNFunctions.raw_test_y)
+            self.datay = DNNFunctions.raw_test_y
+        self.showPhoto(self.lengthx, self.datax)
+        
+
     """
     def filterPhotos(self):
         filterText, done1 = QtWidgets.QInputDialog.getText(self, 'Filtering', 'Enter text to filter:')
