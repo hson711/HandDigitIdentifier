07bc86db1102ff1e1141e75b7236e82e76b48103
Koe2200 <79816353+Koe2200@users.noreply.github.com>
2022-05-02 16:35:14 +1200

    Created comments for drop down datasets.py

--- scripts/dropDownDatasets.py
+++ scripts/dropDownDatasets.py
@@ -7,13 +7,17 @@ from importDatasetScreen import importDatasetScreen
 import contextlib
 from photoViewerWindow import photoViewerWindow
 
+#QMainWindow Class that allows users to choose what specific dataset to use
 class dropDownDatasets(QMainWindow):
+
+    #Initializes an instance
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('Chose dataset to use')
         self.combobox = QComboBox()
 
+        #Adds the datasets keys to the combobox
         if DNNFunctions.keys != NULL:
             for k in DNNFunctions.keys:
                 self.combobox.addItem(k)
@@ -25,17 +29,23 @@ class dropDownDatasets(QMainWindow):
         self.combobox.currentTextChanged.connect(self.text_changed)
         self.combobox.activated.connect(self.text_changed)
 
+    #Function that is called when a combobox option is selected
+    #It closes the current window and then calls the importDataSets function
     def text_changed(self):
         with contextlib.redirect_stdout(None):
             self.s = self.combobox.currentText()
         self.importDataSets()
         self.close()
 
+    #When called it creates an instance of the importing dataset window and shows it
     def importDataSets(self):
         self.importDatasetScreen = importDatasetScreen(self.s)
         self.importDatasetScreen.show()
 
+#Class that allows the user to select the training or testing set to see photos of
 class dropDownPhotoViewer(QMainWindow):
+
+    #Initializing an instance
     def __init__(self):
         super().__init__()
 
@@ -53,11 +63,14 @@ class dropDownPhotoViewer(QMainWindow):
         self.combobox.currentTextChanged.connect(self.text_changed)
         self.combobox.activated.connect(self.text_changed)
 
+    #Function that is called when a combobox option is selected
+    #It closes the current window and then calls the photoViewerWindow function
     def text_changed(self):
         with contextlib.redirect_stdout(None):
             self.s = self.combobox.currentText()
         self.photoViewerWindow()
         self.close()
     
+    #When called it creates an instance of the photo viewer window and shows it
     def photoViewerWindow(self):
         self.w = photoViewerWindow(self.s)
