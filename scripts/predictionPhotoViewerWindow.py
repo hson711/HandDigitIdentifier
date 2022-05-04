4a8acaab1c01e74f09fb304446405ea8965b076b
Koe2200 <79816353+Koe2200@users.noreply.github.com>
2022-05-04 12:41:59 +1200

    Updated photo viewer window

--- scripts/predictionPhotoViewerWindow.py
+++ scripts/predictionPhotoViewerWindow.py
@@ -15,34 +15,47 @@ import numpy as np
 
 class predictionPhotoViewerWindow(QMainWindow):
 
-    def __init__(self,pathFileArray):
+    def __init__(self,set):
 
         super().__init__()
-        self.initUI(pathFileArray)
-        
-    def initUI(self,pathFileArray):
+        self.initUI(set)
 
-        self.baseDir = os.getcwd()
-        self.pathFileArray = pathFileArray
+    def initUI(self,set):
 
-        #Counter set-up
+        #Setup of variables
+        self.filterText = None
+        self.scroll = QScrollArea()
+        self.widget = QWidget() 
+        self.layout = QGridLayout()
         self.i = 0
         self.j = 0
         self.z = 0
-
-        #Layout set-up
-        self.layout = QGridLayout()
-        self.widget = QWidget()
         self.layout.setSpacing(0)
+        self.pathFileArray = set
+
+
         self.widget.setLayout(self.layout)
+
+        #Scroll Area Properties
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setWidget(self.widget)
+        self.scrollBar = self.scroll.verticalScrollBar()
+
+        self.setCentralWidget(self.scroll)
+
+        self.setGeometry(600, 100, 933, 600)
+        self.setFixedSize(933,600)
+        self.setWindowTitle('Prediction Dataset Viewer')
 
         self.predictShow(self.pathFileArray)
         self.show()
 
     def predictShow(self,pathFileArray):
         for i in pathFileArray:
-            pix = QPixmap(pathFileArray[i])
-            predictionText = str(DNNFunctions.predict(pathFileArray[i]))
+            pix = QPixmap(i)
+            predictionText = str(DNNFunctions.predict(i))
             if not pix.isNull():
                     imageLabel = QLabel(self, text= predictionText)
                     imageLabel.setPixmap(pix)
