e9f7c24117c3c0921e505293e9b00eaa97d16e60
hson711 <80075952+hson711@users.noreply.github.com>
2022-05-04 21:09:29 +1200

    changes to DNNFunctions

--- scripts/DNNFunctions.py
+++ scripts/DNNFunctions.py
@@ -4,26 +4,19 @@ from asyncio.windows_events import NULL
 from importlib.resources import path
 import pathlib
 from traceback import print_list
-from typing_extensions import Self
 import keras
 from keras.models import load_model
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Dropout, Flatten, BatchNormalization
-import matplotlib.pyplot as plt
 import numpy as np
-import struct
-import random
-import gzip
-import gzip
 from extra_keras_datasets import emnist
 from pandas import array
 from scipy import io as sio
 from PIL.ImageQt import ImageQt 
-from PIL import Image
 from PyQt5.QtGui import *
 from PyQt5 import *
 import cv2
-import numpy, scipy.io, zipfile
+import numpy, zipfile
 from PIL.ImageQt import ImageQt
 from torchvision.transforms import ToPILImage
 from scipy import *
@@ -38,6 +31,10 @@ class DNNFunctions():
     (raw_train_x, raw_train_y), (raw_test_x, raw_test_y) = (NULL, NULL), (NULL, NULL)
     (train_x, train_y), (test_x, test_y) = (NULL, NULL), (NULL, NULL)
     predictedValue = NULL
+    model = NULL
+    loaded_model = NULL
+    loaded_model_results = NULL
+
 
     #Sets up the path to the downloaded file if its downloaded
     w = NULL
@@ -47,23 +44,18 @@ class DNNFunctions():
     pathFile = os.path.join(location, file)\
     #Initializes keys
     keys = {'matlab/emnist-balanced.mat', 'matlab/emnist-byclass.mat', 'matlab/emnist-bymerge.mat', 'matlab/emnist-digits.mat', 'matlab/emnist-letters.mat', 'matlab/emnist-mnist.mat'}
+    #If file is downloaded loads keys
+    if os.path.isfile(pathFile) == True:
+            numpyObject = numpy.load(pathFile)
+            keys = numpyObject.keys()
     #Preset labels
     labels = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
-    model = NULL
-    loaded_model = NULL
-    loaded_model_results = NULL
+    
     
     def __init__(self):
         return
 
-    #Rotates Image
-    ##NOT CURRENTLY USED
-    def rotate(image):
-        image = np.fliplr(image)
-        image = np.rot90(image)
-        return image
-    
     #Function that is passed a string of what class of data set to use and downloads the dataset and activates the target class of data
     def loadEMNIST(string):
         #If file is not downloaded, download and load the by class data
@@ -191,6 +183,7 @@ class DNNFunctions():
         DNNFunctions.model.add(Dense(128, activation='relu'))
         DNNFunctions.model.add(Dense(62, activation='softmax'))
         
+    #Load model --> takes in model path and returns true if model exists
     def model_load(model_path):
         try:
             DNNFunctions.loaded_model = load_model(model_path)
@@ -198,6 +191,8 @@ class DNNFunctions():
         except IOError as ioe:
             return False
     
+
+    #Predict using the loaded model and return the prediction
     def predict(img_path):
         DNNFunctions.model = DNNFunctions.loaded_model
         image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE )
@@ -213,102 +208,3 @@ class DNNFunctions():
         return(DNNFunctions.labels[resultLabels[0]])
         
         
-
-
-
-#import numpy as np
-#>>> import Image
-#>>> im = Image.fromarray(np.random.randint(0,256,size=(100,100,3)).astype(np.uint8))
-#>>> im.show()
-        
-
-    # def save_model(self):
-    #     folderpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Folder')
-
-    #     #need to incorporate user input !!
-    #     file_name = "model_test"
-    #     self.model.save(folderpath+file_name+'.h')
-
-
-"""
-    (raw_train_x, raw_train_y), (raw_test_x, raw_test_y) = emnist.load_data(type='byclass')
-
-    plt.imshow(raw_train_x[0], cmap='gray')
-    plt.colorbar()
-    plt.show()
-
-    #Labels used to define the values of the images
-    labels = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-
-    # fig, ax = plt.subplots()
-
-    # for x in range(raw_train_x.shape[0]):
-    #     ax.clear()
-    #     ax.imshow([i for i in 255 - raw_train_x[x]], cmap='gray')
-    #     title = 'label = %d = %s' % (raw_train_y[x], labels[raw_train_y[x]])
-    #     ax.set_title(title, fontsize=20)
-    #     plt.pause(1)
-
-    print('train_x_shape: ', raw_train_x.shape)
-    print('train_y_shape: ', raw_train_y.shape)
-    print('test_x_shape: ', raw_test_x.shape)
-    print('test_y_shape: ', raw_test_y.shape)
-
-    train_x = raw_train_x.reshape(len(raw_train_x), 784)
-    test_x = raw_test_x.reshape(len(raw_test_x), 784)
-
-    print('New train_x: ', train_x.shape)
-    print('New test_x: ', test_x.shape)
-
-
-    train_x = train_x.astype('float32')
-    test_x = test_x.astype('float32')
-
-    train_x = train_x/255
-    test_x = test_x/255
-
-
-    # plt.imshow(train_x.reshape(len(train_x),28,28)[112358], cmap='gray')
-    # plt.colorbar()
-    # plt.show()
-
-    train_y = keras.utils.np_utils.to_categorical(raw_train_y)
-    test_y = keras.utils.np_utils.to_categorical(raw_test_y)
-
-    print(raw_train_y[4], train_y[4])
-    print(train_x.shape)
-    print(test_y.shape)
-
-    model = keras.models.Sequential()
-    model.add(Dense(16,input_dim = 784, activation='relu'))
-    model.add(Dense(32,activation='relu'))
-    model.add(Dense(20,activation='relu'))
-    model.add(Dense(62,activation='softmax'))
-
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-
-    model.fit(train_x, train_y, epochs=1, batch_size=100)
-
-    # Getting the loss and accuracy percentages    
-    # results = model.evaluate(test_x, test_y)
-    # print("Loss & Accuracy:")
-    # print(results[0]*100, results[1]*100)
-
-
-    #Checking the prediction of 10 randomly selected values
-    random.seed(3131)
-    sample = np.arange(raw_test_x.shape[0])
-    np.random.shuffle(sample)
-    sample = sample[0:10]
-
-    results = np.round(model.predict(test_x[sample], verbose=1), decimals=2)
-    resultLabels = np.argmax(results, axis = 1)
-
-    fig = plt.figure(figsize=(15,8))
-    for i in range(10):
-        fig.add_subplot(2,5,i+1, aspect='equal')
-        plt.imshow(raw_test_x[sample[i]], cmap='gray')
-        plt.title('Pred: {}'.format(labels[resultLabels[i]]))
-    plt.show()
-    """
